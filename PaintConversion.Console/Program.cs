ba4583a19fa2941c6bd0cd916c2f5bdee192b089
Andrew Black <a.black@lancaster.ac.uk>
2024-10-22 07:22:29 +0100

    Add Serilog

--- PaintConversion.Console/Program.cs
+++ PaintConversion.Console/Program.cs
@@ -1,4 +1,36 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-Console.WriteLine("Welcome to the Paint Conversion App");
-Console.ReadLine();
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger.Information("Application Starting");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) => 
+    {
+
+    })
+    .UseSerilog()
+    .Build();
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}", optional: true)
+    .AddEnvironmentVariables();
+}
+
+public class GreetingService
+{
+    
+}
